fa56ca5d13342242ff28d79605f45b780aae7305
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-06-09 14:30:00 +0300

    Updated

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -8,6 +8,9 @@ import { useState } from "react";
 
 export default function ProfilePage() {
   const { user, logout } = useAuth();
+  const [activePage, setActivePage] = useState("postari");
+  const [passwordChangeActive, setPasswordChangeActive] = useState(false);
+  const [deleteAccountActive, setDeleteAccountActive] = useState(false);
   const router = useRouter();
 
   const handleLogout = () => {
@@ -27,7 +30,7 @@ export default function ProfilePage() {
     if (user?.lostfoundID) {
       navigator.clipboard.writeText(user.lostfoundID);
       setCopied(true);
-      setTimeout(() => setCopied(false), 5000);
+      setTimeout(() => setCopied(false), 2000);
     }
   };
 
@@ -98,7 +101,80 @@ export default function ProfilePage() {
             Ieși din cont
           </button>
         </div>
-        <div className={styles.posts_and_settings}>123</div>
+        <div className={styles.posts_and_settings}>
+          <div className={styles.menu}>
+            <button
+              onClick={() => {
+                setActivePage("postari");
+                setPasswordChangeActive(false);
+                setDeleteAccountActive(false);
+              }}
+              className={activePage === "postari" ? styles.active : ""}
+            >
+              Postări
+            </button>
+            <button
+              onClick={() => setActivePage("setari")}
+              className={activePage === "setari" ? styles.active : ""}
+            >
+              Setări cont
+            </button>
+          </div>
+          {activePage === "postari" && (
+            <div>Aici vor aparea postarile dumneavoastra</div>
+          )}
+          {activePage === "setari" && (
+            <div className={styles.settings_container}>
+              <div className={styles.settings}>
+                <div
+                  className={styles.settingstext}
+                  onClick={() => setPasswordChangeActive(!passwordChangeActive)}
+                >
+                  Schimbare parolă
+                  <Image
+                    src="/icons/arrow-right.svg"
+                    alt="Arrow Right Icon"
+                    style={passwordChangeActive ? { rotate: "90deg" } : {}}
+                    width={20}
+                    height={20}
+                  />{" "}
+                </div>
+                <div
+                  className={`${styles.settingsform} ${
+                    passwordChangeActive ? styles.active : ""
+                  }`}
+                >
+                  <input type="password" placeholder="Parolă veche" />
+                  <input type="password" placeholder="Parolă nouă" />
+                  <input type="password" placeholder="Confirmă parola" />
+                  <button>Salvează</button>
+                </div>
+              </div>
+              <div
+                className={styles.settings}
+                style={{
+                  color: "red",
+                  border: "1px solid rgba(255, 0, 0, 0.5)",
+                  opacity: "0.7",
+                }}
+              >
+                <div
+                  className={styles.settingstext}
+                  onClick={() => setDeleteAccountActive(!deleteAccountActive)}
+                >
+                  Ștergere cont
+                  <Image
+                    src="/icons/arrow-right-red.svg"
+                    alt="Red Arrow Right Icon"
+                    style={deleteAccountActive ? { rotate: "90deg" } : {}}
+                    width={20}
+                    height={20}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </main>
   );
