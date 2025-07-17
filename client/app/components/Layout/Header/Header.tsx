2452ec4aba67ed6a321d201585f01a355db6be8d
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-07-17 21:57:52 +0300

    Update Header.tsx

--- client/app/components/Layout/Header/Header.tsx
+++ client/app/components/Layout/Header/Header.tsx
@@ -86,8 +86,8 @@ export default function Header() {
               <Image
                 src="/icons/search-yellow.svg"
                 alt="Yellow Search Icon"
-                width={21}
-                height={21}
+                width={23}
+                height={23}
                 draggable={false}
               />
               Caută anunțuri
@@ -155,8 +155,8 @@ export default function Header() {
             <Image
               src="/icons/search-yellow.svg"
               alt="Yellow Search Icon"
-              width={24}
-              height={24}
+              width={25}
+              height={25}
               draggable={false}
             />
             Caută anunțuri
